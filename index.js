70ec5e658d378e565c3458ef0f6a98a9bf2bb8f5
etb <etb@etbs-MacBook-Pro.local>
2018-07-04 17:55:51 +0100

    Create example API with twitter authentication using express and passportjs

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const config = require('./config');
+
+// Configure Twitter authentication strategy to be used by Passport.
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: config.twitterConsumerKey,
+      consumerSecret: config.twitterConsumerSecret,
+      callbackURL: 'http://127.0.0.1:8080/auth/twitter/callback',
+    },
+    (token, tokenSecret, profile, done) => {
+      console.log(`User connected! \n
+        Token: ${token} \n
+        Token Secret: ${tokenSecret}
+        Profile: ${JSON.stringify(profile)}`);
+
+      // FIXME return the actual user and not its Twitter profile.
+      done(null, profile);
+    },
+  ),
+);
+
+// User serialization/deserialization.
+//
+// FIXME Serialize should retrieve the local user ID and deserialize
+// should get and retrieve the actual user object from the its ID.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Create the express application.
+const app = express();
+
+// Configure middleware for logging and session managing.
+//app.use(express.static("public"));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: config.sessionSecret, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/', /*(req, res) => { res.send('Hello world!'); });*/
+  // TODO this function to verify user and eventually roles as well
+  // should be defined separately.
+  (req, res) => {
+    if (req.user) {
+      res.send('Hello world!')
+    } else {
+      res.redirect('/login/twitter');
+    };
+  },
+);
+
+app.get('/login/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login/twitter' }),
+  (req, res) => {
+    res.redirect('/');
+  },
+);
+
+// Start listening for incoming connection.
+app.listen(8080, () => {
+  console.log('app listening on port 8080!');
+});
