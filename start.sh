c92311c05cc1bb9af85165acf10b3afd1e27135d
notsag-dev <a.gaston.gimenez@gmail.com>
2018-07-31 12:03:42 +0100

    Change start script because of the app folder name change.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker build --no-cache -t consensusclubs/api-consensus-clubs:v1.0.0 app/
+docker build --no-cache -t consensusclubs/api-consensus-clubs:v1.0.0 api/
 kubectl create -f secrets.yaml
 kubectl create -f eos.yaml
 kubectl create -f node.yaml
