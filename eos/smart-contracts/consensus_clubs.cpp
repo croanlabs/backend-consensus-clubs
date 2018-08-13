c94fcbadecc5b96f8b57bee1e0288a76f2298454
notsag-dev <a.gaston.gimenez@gmail.com>
2018-08-13 21:45:32 +0100

    Add tables and function to insert values into them

--- eos/smart-contracts/consensus_clubs.cpp
+++ eos/smart-contracts/consensus_clubs.cpp
@@ -11,33 +11,62 @@ void consensus_clubs::newuser(string name, uint64_t unopinionated_merits) {
   });
 }
 
-void consensus_clubs::newclub(string name,
-    string description,
-    string type,
-    string avatar_url) {
-  clubs.emplace(_self, [&](auto& new_club) {
-    new_club.id = clubs.available_primary_key();
-    new_club.name = name;
-    new_club.description = description;
-    new_club.type = type;
-    new_club.avatar_url = avatar_url;
-  });
-}
-
-void consensus_clubs::newmember(uint64_t user_id, uint64_t club_id) {
-  memberships.emplace(_self, [&](auto& new_membership) {
-    new_membership.id = memberships.available_primary_key();
-    new_membership.user_id = user_id;
-    new_membership.club_id = club_id;
-  });
-}
-
-void consensus_clubs::newpoll(uint64_t club_id, string question, string description) {
+void consensus_clubs::newpoll(string question, string description) {
   polls.emplace(_self, [&](auto& new_poll) {
     new_poll.id = polls.available_primary_key();
-    new_poll.club_id = club_id;
     new_poll.question = question;
     new_poll.description = description;
+  });
+}
+
+void consensus_clubs::newcandidate(
+    uint64_t poll_id,
+    string name,
+    string description,
+    string avatar_url,
+    uint32_t total_tokens_confidence,
+    uint32_t total_tokens_no_confidence,
+    vector<token_holder> token_holders_confidence,
+    vector<token_holder> token_holders_no_confidence) {
+      candidates.emplace(_self, [&](auto& new_candidate) {
+        new_candidate.id = candidates.available_primary_key();
+        new_candidate.poll_id = poll_id;
+        new_candidate.name = name;
+        new_candidate.description = description;
+        new_candidate.avatar_url = avatar_url;
+        new_candidate.total_tokens_confidence = total_tokens_confidence;
+        new_candidate.total_tokens_no_confidence = total_tokens_no_confidence;
+        new_candidate.token_holders_confidence = token_holders_confidence;
+        new_candidate.token_holders_no_confidence = token_holders_no_confidence;
+      });
+}
+
+void consensus_clubs::newopinion(
+    uint64_t user_id,
+    uint64_t poll_candidate_id, 
+    string opinion_type, 
+    uint32_t commitment) {
+  opinions.emplace(_self, [&](auto& new_opinion) {
+    new_opinion.id = opinions.available_primary_key();
+    new_opinion.user_id = user_id;
+    new_opinion.poll_candidate_id = poll_candidate_id;
+    new_opinion.opinion_type = opinion_type;
+    new_opinion.commitment = commitment;
+  });
+}
+
+void consensus_clubs::newaction(uint64_t user_id,
+    uint64_t poll_candidate_id,
+    string action_type,
+    double amount,
+    string date) {
+  actions.emplace(_self, [&](auto& new_action) {
+    new_action.id = actions.available_primary_key();
+    new_action.user_id = user_id;
+    new_action.poll_candidate_id = poll_candidate_id;
+    new_action.action_type = action_type;
+    new_action.amount = amount;
+    new_action.date = date;
   });
 }
 
--- eos/smart-contracts/consensus_clubs.hpp
+++ eos/smart-contracts/consensus_clubs.hpp
@@ -10,21 +10,41 @@ class consensus_clubs : public eosio::contract {
     consensus_clubs(account_name self):
       contract(self),
       users(_self, _self),
-      clubs(_self, _self),
-      memberships(_self, _self),
-      polls(_self, _self) {}
+      polls(_self, _self),
+      candidates(_self, _self),
+      opinions(_self, _self),
+      actions(_self, _self) {}
 
     /// @abi action
     void newuser(string name, uint64_t unopinionated_merits);
 
     /// @abi action
-    void newclub(string name, string description, string type, string avatar_url);
+    void newpoll(string question, string description);
+
+    struct token_holder {
+      uint64_t user_id;
+      uint32_t amount; 
+    };
 
     /// @abi action
-    void newmember(uint64_t user_id, uint64_t club_id);
+    void newcandidate(uint64_t poll_id,
+        string name,
+        string description,
+        string avatar_url,
+        uint32_t total_tokens_confidence,
+        uint32_t total_tokens_no_confidence,
+        vector<token_holder> token_holders_confidence,
+        vector<token_holder> token_holders_no_confidence);
 
     /// @abi action
-    void newpoll(uint64_t club_id, string question, string description);
+    void newopinion(uint64_t user_id, uint64_t poll_candidate_id, string opinion_type, uint32_t commitment);
+
+    /// @abi action
+    void newaction(uint64_t user_id,
+        uint64_t poll_candidate_id,
+        string action_type,
+        double amount,
+        string date);
 
   private:
     // @abi table users i64
@@ -37,43 +57,65 @@ class consensus_clubs : public eosio::contract {
       EOSLIB_SERIALIZE(user, (id)(name)(unopinionated_merits))
     };
 
-    // @abi table clubs i64
-    struct club {
-      uint64_t id;
-      string name;
-      string description;
-      string type;
-      string avatar_url;
-
-      uint64_t primary_key() const { return id; };
-      EOSLIB_SERIALIZE(club, (id)(name)(description)(type)(avatar_url))
-    };
-
-    // @abi table memberships i64
-    struct membership {
-      uint64_t id;
-      uint64_t user_id;
-      uint64_t club_id;
-
-      uint64_t primary_key() const { return id; };
-      EOSLIB_SERIALIZE(membership, (id)(user_id)(club_id))
-    };
-
     // @abi table polls i64
     struct poll {
       uint64_t id;
-      uint64_t club_id;
       string question;
       string description;
 
       uint64_t primary_key() const { return id; };
-      EOSLIB_SERIALIZE(poll, (id)(club_id)(question)(description))
+      EOSLIB_SERIALIZE(poll, (id)(question)(description))
+    };
+
+    // @abi table candidates i64
+    struct candidate {
+      uint64_t id;
+      uint64_t poll_id;
+      string name;
+      string description;
+      string avatar_url;
+      uint32_t total_tokens_confidence;
+      uint32_t total_tokens_no_confidence;
+      vector<token_holder> token_holders_confidence;
+      vector<token_holder> token_holders_no_confidence;
+
+      uint64_t primary_key() const { return id; };
+      EOSLIB_SERIALIZE(candidate,
+          (id)(poll_id)(name)(description)(avatar_url)
+          (total_tokens_confidence)(total_tokens_no_confidence)
+          (token_holders_confidence)(token_holders_no_confidence))
+    };
+
+    // @abi table opinions i64
+    struct opinion {
+      uint64_t id;
+      uint64_t user_id;
+      uint64_t poll_candidate_id;
+      string opinion_type;
+      uint32_t commitment;
+
+      uint64_t primary_key() const { return id; };
+      EOSLIB_SERIALIZE(opinion, (id)(user_id)(poll_candidate_id)(opinion_type)(commitment))
+    };
+
+    // @abi table actions i64
+    struct action {
+      uint64_t id;
+      uint64_t user_id;
+      uint64_t poll_candidate_id;
+      string action_type;
+      double amount;
+      string date;
+
+      uint64_t primary_key() const { return id; };
+      EOSLIB_SERIALIZE(action, (id)(user_id)(poll_candidate_id)(action_type)(amount)(date))
     };
 
     eosio::multi_index<N(users), user> users;
-    eosio::multi_index<N(clubs), club> clubs;
-    eosio::multi_index<N(memberships), membership> memberships;
     eosio::multi_index<N(polls), poll> polls;
+    eosio::multi_index<N(candidates), candidate> candidates;
+    eosio::multi_index<N(opinions), opinion> opinions;
+    eosio::multi_index<N(actions), action> actions;
 };
 
-EOSIO_ABI(consensus_clubs, (newuser)(newclub)(newmember))
+EOSIO_ABI(consensus_clubs, (newuser)(newpoll)(newcandidate)(newopinion)(newaction))
