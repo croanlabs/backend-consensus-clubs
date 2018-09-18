9ab0ae297453880a3d9a3a6590c9f06e00fa0717
notsag-dev <a.gaston.gimenez@gmail.com>
2018-09-18 19:03:48 +0100

    Add tests to the notification creation and change candidate notification to be instead poll notification

--- api/models/generalNotification.js
+++ api/models/generalNotification.js
@@ -1,24 +1,27 @@
-module.exports = (sequelize, DataTypes) => {
-  const GeneralNotification = sequelize.define(
-    'GeneralNotification',
-    {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      notificationId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Notifications',
-          referencesKey: 'id',
-        },
+const db = require('../config/database');
+const Sequelize = require('sequelize');
+
+let GeneralNotification = db.define(
+  'GeneralNotification',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    notificationId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Notifications',
+        referencesKey: 'id',
       },
     },
-    {},
-  );
-  GeneralNotification.associate = (models) => {
-    GeneralNotification.belongsTo(models.Notification);
-  };
-  return GeneralNotification;
+  },
+  {},
+);
+
+GeneralNotification.associate = models => {
+  GeneralNotification.belongsTo(models.Notification);
 };
+
+module.exports = GeneralNotification;
--- api/models/userNotification.js
+++ api/models/userNotification.js
@@ -1,27 +1,30 @@
-module.exports = (sequelize, DataTypes) => {
-  let UserNotification = sequelize.define(
-    'UserNotification',
-    {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      notificationId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Notifications',
-          referencesKey: 'id',
-        },
-      },
-      userId: DataTypes.INTEGER,
-      createdAt: Sequelize.DATE,
-      updatedAt: Sequelize.DATE,
+const db = require('../config/database');
+const Sequelize = require('sequelize');
+
+let UserNotification = db.define(
+  'UserNotification',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {},
-  );
-  UserNotification.associate = models => {
-    UserNotification.belongsTo(models.Notification);
-  };
-  return UserNotification;
+    notificationId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Notifications',
+        referencesKey: 'id',
+      },
+    },
+    userId: Sequelize.INTEGER,
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+  },
+  {},
+);
+
+UserNotification.associate = models => {
+  UserNotification.belongsTo(models.Notification);
 };
+
+module.exports = UserNotification;
